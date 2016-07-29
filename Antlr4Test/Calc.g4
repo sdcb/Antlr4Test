6db81e0c29fe67f10e280f794503f1a4b6a79574
flysha <flysha@live.com>
2016-07-30 00:42:07 +0800

    fucking cool

--- Antlr4Test/Calc.g4
+++ Antlr4Test/Calc.g4
@@ -1,34 +1,47 @@
 grammar Calc;
 
 root
-	: expression
+	: (statement EOL)*
+	;
+
+statement
+	: SYNTAX '=' expression     #Assign
+	| SYNTAX '(' expression ')' #Call
 	;
 
 expression
 	: '(' expression ')'                           #Parenthesis
-	| FUNC '(' expression ')'                      #Function
-	| FUNC '(' expression ',' expression ')'       #BinaryFunction
+	| SYNTAX '(' expression ')'                    #Function
+	| SYNTAX '(' expression ',' expression ')'     #BinaryFunction
 	| expression operator = ('*' | '/') expression #Binary
 	| expression operator = ('+' | '-') expression #Binary
 	| NUMBER                                       #Number
+	| SYNTAX									   #Variable
 	;
+
+BlockComment
+    :   '/*' .*? '*/'
+        -> skip
+    ;
+
+LineComment
+    :   '//' ~[\r\n]*
+        -> skip
+    ;
 
 ADD: '+';
 SUB: '-';
 MUL: '*';
 DIV: '/';
+EOL: ';';
 
 NUMBER
 	: '-'? [0-9]+ ('.' [0-9]+)?
 	;
 
-FUNC
+SYNTAX
 	: [a-zA-Z]? [a-zA-Z0-9]*
 	;
-
-//BINFUNC
-//	: [a-zA-Z]? [a-zA-Z0-9]*
-//	;
 
 WS
 	: [ \r\n\t] + -> channel (HIDDEN)
--- Antlr4Test/Program.cs
+++ Antlr4Test/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Antlr4.Runtime.Misc;
 using Antlr4.Runtime.Tree;
 using static Antlr4Test.CalcParser;
+using System.IO;
 
 namespace Antlr4Test
 {
@@ -14,18 +15,75 @@ namespace Antlr4Test
     {
         static void Main(string[] args)
         {
-            var input = "abs(-3) + 0.5 * pow(2, 4 / 2) + 3 / 4";
+            var input = File.ReadAllText("input.txt");
             var inputStream = new AntlrInputStream(input);
             var lexer = new CalcLexer(inputStream);
             var tokenStream = new CommonTokenStream(lexer);
             var parser = new CalcParser(tokenStream);
 
-            var visit = new Visitor();
-            Console.WriteLine(visit.Visit(parser.root()));
+            var visit = new StatementVisitor();
+            //Console.WriteLine(parser.root().GetText());
+            visit.Visit(parser.root());
         }
 
-        class Visitor : CalcBaseVisitor<double>
+        class StatementVisitor : CalcBaseVisitor<double>
         {
+            Dictionary<string, double> vars = new Dictionary<string, double>();
+            
+            public override double VisitAssign([NotNull] AssignContext context)
+            {
+                var syntax = context.GetChild(0).GetText();
+                var exp = context.GetChild<ExpressionContext>(0);
+                SetVar(syntax, EvalStatement(exp));
+                return 0;
+            }
+            
+            public override double VisitCall([NotNull] CallContext context)
+            {
+                var syntax = context.GetChild(0).GetText();
+                var exp = EvalStatement(context.GetChild<ExpressionContext>(0));
+
+                switch (syntax)
+                {
+                    case "write":
+                        Console.WriteLine(exp);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(syntax));
+                }
+                return 0;
+            }
+
+            private void SetVar([NotNull]string syntax, double value)
+            {
+                vars[syntax] = value;
+            }
+
+            private double GetVar([NotNull]string syntax)
+            {
+                return vars[syntax];
+            }
+
+            public double EvalStatement([NotNull]ExpressionContext expression)
+            {
+                return new ExpressionVisitor(GetVar).Visit(expression);
+            }
+        }
+
+        class ExpressionVisitor : CalcBaseVisitor<double>
+        {
+            public Func<string, double> VarGetter { get; }
+
+            public ExpressionVisitor([NotNull]Func<string, double> varGetter)
+            {
+                VarGetter = varGetter;
+            }
+
+            public override double VisitParenthesis([NotNull] ParenthesisContext context)
+            {
+                return Visit(context.GetChild<ExpressionContext>(0));
+            }
+
             public override double VisitNumber([NotNull] CalcParser.NumberContext context)
             {
                 return double.Parse(context.GetText());
@@ -79,6 +137,12 @@ namespace Antlr4Test
                     default:
                         throw new ArgumentOutOfRangeException(nameof(op));
                 }
+            }
+
+            public override double VisitVariable([NotNull] VariableContext context)
+            {
+                var syntax = context.GetText();
+                return VarGetter(syntax);
             }
         }
     }
